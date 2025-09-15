89d76107156b0712ba707afd30c586e7c50e6c49
Ann Matt <anmatyushkova@gmail.com>
2025-09-15 18:14:04 +0300

    final setting of alias

--- shopping cart/vite.config.ts
+++ shopping cart/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 
 // https://vite.dev/config/
@@ -7,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': './src',
+      '@': path.resolve(__dirname,'src'),
     },
   },
 })
